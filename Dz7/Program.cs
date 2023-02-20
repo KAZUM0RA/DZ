9cac621eb5ad859463a1a33c730fd36127e9657f
KAZUM0RA <124499055+KAZUM0RA@users.noreply.github.com>
2023-02-20 15:53:28 +0200

    dz v2

--- Dz7/Program.cs
+++ Dz7/Program.cs
@@ -3,32 +3,44 @@ using VpuDotnet.Contexts;
 using System.Text.Json;
 
 var context = new CarContext();
-context.Create(new Car
-{
-    markName = "Ford",
-    model = "Cadette",
-    year = 1984,
-});
-context.Create(new Car
-{
-    markName = "Opel",
-    model = "Astra",
-    year = 2013,
-});
-context.Create(new Car
-{
-    markName = "Jeep",
-    model = "Classic",
-    year = 2001,
-});
-context.Create(new Car
-{
-    markName = "Fiat",
-    model = "Doblo",
-    year = 2009,
-});
+// context.Create(new Car
+// {
+//     markName = "Ford",
+//     model = "Cadette",
+//     year = 1984,
+// });
+// context.Create(new Car
+// {
+//     markName = "Opel",
+//     model = "Astra",
+//     year = 2013,
+// });
+// context.Create(new Car
+// {
+//     markName = "Jeep",
+//     model = "Classic",
+//     year = 2001,
+// });
+// context.Create(new Car
+// {
+//     markName = "Fiat",
+//     model = "Doblo",
+//     year = 2009,
+// });
 
 
 var Allcars = context.GetAll();
 foreach (var cars in Allcars)
 Console.WriteLine($"{cars.markName}\n{cars.model}\n{cars.year}\n" );
+
+// context.Save();
+
+// context.Create(new Car {
+//     markName = "Ford ",
+//     model = "Mondeo",
+//     year = 2000
+// });
+context.Save();
+
+
+
